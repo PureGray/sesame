a2ea2ca3836fb124dde7375a3430e4f0ba2a58d4
renlanchuke <1065730562@qq.com>
2019-04-05 09:58:38 -0700

    serialNumber

---
+++ sesame-service/src/main/java/org/sesame/service/entity/SerialNumber.java
@@ -0,0 +1,66 @@
+package org.sesame.service.entity;
+
+public class SerialNumber {
+	private long version;
+	private long type;
+	private long machineId;
+	private long millisecond;
+	private long sequence;
+
+	public SerialNumber(long version, long type, long machineId, long millisecond, long sequence) {
+		this.version = version;
+		this.type = type;
+		this.machineId = machineId;
+		this.millisecond = millisecond;
+		this.sequence = sequence;
+	}
+
+	public SerialNumber() {
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sBuilder = new StringBuilder();
+		
+		sBuilder.append("{version:").append(version).append(",").append("type:").append(type).append(",")
+				.append("machineId").append(machineId).append(",").append("millisecond").append(millisecond).append(",")
+				.append("sequence").append(sequence).append("}");
+		
+		return sBuilder.toString();
+	}
+
+	public static class Builder {
+		private SerialNumber sNumber = new SerialNumber();
+
+		public Builder version(long v) {
+			sNumber.version = v;
+			return this;
+		}
+
+		public Builder type(long t) {
+			sNumber.type = t;
+			return this;
+		}
+
+		public Builder machineId(long m) {
+			sNumber.machineId = m;
+			return this;
+		}
+
+		public Builder millisecond(long m) {
+			sNumber.machineId = m;
+			return this;
+		}
+
+		public Builder sequence(long s) {
+			sNumber.sequence = s;
+			return this;
+		}
+
+		public SerialNumber build() {
+			return sNumber;
+		}
+
+	}
+
+}
