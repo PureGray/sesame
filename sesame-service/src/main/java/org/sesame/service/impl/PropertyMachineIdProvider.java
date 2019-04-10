852641951020fb72c1bbd1aa591b5353856c732e
renlanchuke <1065730562@qq.com>
2019-04-10 08:34:01 -0700

    propertyMachineIdProvider

---
+++ sesame-service/src/main/java/org/sesame/service/impl/PropertyMachineIdProvider.java
@@ -0,0 +1,30 @@
+package org.sesame.service.impl;
+
+import org.sesame.service.entity.SerialNumberMeta;
+import org.sesame.service.factory.SerialNumberMetaFac;
+import org.sesame.service.interfaces.MachineIdProvider;
+
+/**
+ * This provide a simple MachineIdProvider which just provides machineId by configure machineId
+ * property & MachineIdBitsMask.
+ * @author renlan
+ *
+ */
+public class PropertyMachineIdProvider implements MachineIdProvider{
+	private long machineId;
+	private SerialNumberMeta snMeta;
+	
+	public PropertyMachineIdProvider() {
+		snMeta=SerialNumberMetaFac.getSerialNumberMeta();
+	}
+
+	public long getMachineId() {
+		
+		return machineId;
+	}
+	
+	public void setMachineId(long machineId) {
+		this.machineId = machineId & snMeta.getMachineIdBitsMask();
+	}
+
+}
--- sesame-service/src/test/java/org/sesame/service/ConfigureServiceImplTest.java
+++ sesame-service/src/test/java/org/sesame/service/ConfigureServiceImplTest.java
@@ -6,16 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.Test;
 
 public class ConfigureServiceImplTest {
-  @Test
-  public void f() {
-	  ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-              "/sesame-service-property-factory-bean.xml");
-	  
-	ConfigureService configureService=(ConfigureService) applicationContext.getBean("configureService");
-	
-	System.out.println(configureService.getType());
+	@Test
+	public void propertyMachineIdProviderTest() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"/property-configure-service-bean.xml");
 
-	System.out.println(configureService.getVersion());
-	
-  }
+		ConfigureService configureService = (ConfigureService) applicationContext.getBean("configureService");
+
+		System.out.println(configureService.getType());
+		System.out.println(configureService.getVersion());
+		System.out.println(configureService.getMachineId());
+
+	}
 }
