f19aecc611ff83da9a22dac00b9c0ee314ec6aaf
renlanchuke <1065730562@qq.com>
2019-04-11 10:14:03 -0700

    converter

---
+++ sesame-service/src/main/java/org/sesame/service/util/SerialNumberConverter.java
@@ -0,0 +1,36 @@
+package org.sesame.service.util;
+
+import org.sesame.service.entity.SerialNumber;
+import org.sesame.service.entity.SerialNumberMeta;
+import org.sesame.service.factory.SerialNumberMetaFac;
+
+public class SerialNumberConverter {
+	private static SerialNumberMeta snMeta = SerialNumberMetaFac.getSerialNumberMeta();
+
+	public static SerialNumber convert(long serialNumber) {
+		long sequence = serialNumber & snMeta.getSequenceBitsMask();
+		serialNumber = serialNumber >>> snMeta.getSequenceBits();
+		long milliseconds = serialNumber & snMeta.getMachineIdBits();
+		serialNumber = serialNumber >>> snMeta.getMachineIdBits();
+		long machineId = serialNumber & snMeta.getMachineIdBitsMask();
+		serialNumber = serialNumber >>> snMeta.getMachineIdBits();
+		long type = serialNumber & snMeta.getTypeBitsMask();
+		serialNumber = serialNumber >>> snMeta.getTypeBits();
+		long version = serialNumber & snMeta.getVersionBitsMask();
+
+		return new SerialNumber.Builder().sequence(sequence).millisecond(milliseconds).machineId(machineId).type(type)
+				.version(version).build();
+	}
+
+	public static long convert(SerialNumber serialNumber) {
+		long sn = -1L;
+
+		sn &= serialNumber.getSequence();
+		sn &= serialNumber.getMillisecond() << snMeta.getgetMillisecondBitsStartPosition();
+		sn &= serialNumber.getMachineId() << snMeta.getMachineIdBitsStartPosition();
+		sn &= serialNumber.getType() << snMeta.getTypeBitsStartPosition();
+		sn &= serialNumber.getVersion() << snMeta.getVersionBitsStartPosition();
+		return sn;
+	}
+
+}
